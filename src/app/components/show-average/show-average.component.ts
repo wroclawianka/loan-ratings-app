384e44ffdd3ac42fd8afb4ad67148233e9b601e4
wroclawianka <dorota.zelga@gmail.com>
2018-05-10 22:22:40 +0200

    receive rating method created

--- src/app/components/show-average/show-average.component.ts
+++ src/app/components/show-average/show-average.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MarketplaceService } from '../../services/marketplace/marketplace.service'
+import { Rating } from '../../models/rating.model';
 
 @Component({
   selector: 'app-show-average',
@@ -8,9 +9,24 @@ import { MarketplaceService } from '../../services/marketplace/marketplace.servi
   styleUrls: ['./show-average.component.css']
 })
 export class ShowAverageComponent implements OnInit {
+  ratings: Array<Rating> = [
+    { value: "AAAAA", label: "A**"},
+    { value: "AAAA", label: "A*"},
+    { value: "AAA", label: "A++"},
+    { value: "AA", label: "A+"},
+    { value: "A", label: "A"},
+    { value: "B", label: "B"},
+    { value: "C", label: "C"},
+    { value: "D", label: "D"}
+  ];
+  rating: Rating;
 
   constructor(private loanService: MarketplaceService) {}
-  
+
   ngOnInit() {
+  }
+
+  receiveRating($event) {
+    this.rating = $event;
   }
 }
