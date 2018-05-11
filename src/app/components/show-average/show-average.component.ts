6039e25d5288c9f2423c202f85dfdc92273a5688
wroclawianka <dorota.zelga@gmail.com>
2018-05-11 23:13:03 +0200

    calculate average

--- src/app/components/show-average/show-average.component.ts
+++ src/app/components/show-average/show-average.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { MarketplaceService } from '../../services/marketplace/marketplace.service'
 import { Rating } from '../../models/rating.model';
+import { Loan } from '../../models/loan.model';
 
 @Component({
   selector: 'app-show-average',
   templateUrl: './show-average.component.html',
   styleUrls: ['./show-average.component.css']
 })
-export class ShowAverageComponent implements OnInit {
+export class ShowAverageComponent  {
   ratings: Array<Rating> = [
     { value: "AAAAA", label: "A**"},
     { value: "AAAA", label: "A*"},
@@ -19,14 +20,19 @@ export class ShowAverageComponent implements OnInit {
     { value: "C", label: "C"},
     { value: "D", label: "D"}
   ];
-  rating: Rating;
+  average : number;
 
-  constructor(private loanService: MarketplaceService) {}
-
-  ngOnInit() {
+  constructor(private marketplaceService: MarketplaceService) {}
+  
+  getLoans(rating) {
+    this.marketplaceService
+    .getLoans(rating.value, ["amount"])
+    .subscribe((loans: Loan[]) => this.average = this.calculateAverage(loans));
   }
 
-  receiveRating($event) {
-    this.rating = $event;
+  calculateAverage(loans : Loan[]) : number{
+    let loansSum : number = 0;
+    loans.forEach(loan => loansSum += loan.amount);
+    return loansSum / loans.length;
   }
 }
