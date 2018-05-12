d0d4bc67d99517b902574ddd2698ba170730630a
wroclawianka <dorota.zelga@gmail.com>
2018-05-12 20:27:24 +0200

    app component styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../styles/styles.scss', './app.component.scss']
 })
 export class AppComponent {
   title = 'Loan Ratings';
