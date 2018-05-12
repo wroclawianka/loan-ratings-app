d6fabc2feb9d4df53129eb699f43c9b66bf9f4ff
wroclawianka <dorota.zelga@gmail.com>
2018-05-12 20:27:51 +0200

    new components added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
-import { SelectRatingComponent } from './components/show-average/select-rating/select-rating.component';
-import { ShowAverageComponent } from './components/show-average/show-average.component';
 import { MarketplaceService } from './services/marketplace/marketplace.service';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MockComponentComponent } from './components/mock-component/mock-component.component';
+import { CalculationsComponent } from './components/calculations/calculations.component';
+import { SelectRatingComponent } from './components/calculations/select-rating/select-rating.component';
+import { ShowResultsComponent } from './components/calculations/show-results/show-results.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectRatingComponent,
-    ShowAverageComponent,
     HeaderComponent,
     FooterComponent,
+    CalculationsComponent,
+    SelectRatingComponent,
+    ShowResultsComponent,
     MockComponentComponent
   ],
   imports: [
