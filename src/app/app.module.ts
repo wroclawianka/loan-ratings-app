fda61e253c81a531f8cce101decf66a3cff5b8b3
wroclawianka <dorota.zelga@gmail.com>
2018-05-12 09:21:39 +0200

    additional components created - header, footer, mock-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { SelectRatingComponent } from './components/show-average/select-rating/select-rating.component';
 import { ShowAverageComponent } from './components/show-average/show-average.component';
 import { MarketplaceService } from './services/marketplace/marketplace.service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MockComponentComponent } from './components/mock-component/mock-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SelectRatingComponent,
-    ShowAverageComponent
+    ShowAverageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MockComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mock-component/mock-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-component',
+  templateUrl: './mock-component.component.html',
+  styleUrls: ['./mock-component.component.css']
+})
+export class MockComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
